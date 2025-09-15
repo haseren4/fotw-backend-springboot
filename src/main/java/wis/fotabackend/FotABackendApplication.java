d13bd1eab5bbb45310b83b30db43ec94d7ce2857
Robert D Walk <haseren4@gmail.com>
2025-09-15 03:28:54 -0400

    Init Commit

---
+++ src/main/java/wis/fotabackend/FotABackendApplication.java
@@ -0,0 +1,13 @@
+package wis.fotabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotABackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FotABackendApplication.class, args);
+    }
+
+}
